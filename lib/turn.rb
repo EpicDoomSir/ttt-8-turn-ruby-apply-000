9fa440d4e95171c27a815681716797bc2636e9ff
Eitan Charytan-Spitz <eitands@gmail.com>
2020-09-15 14:08:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,6 +21,6 @@ end
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
-  else
+  elsif board[index] == "X" || board[index] == "O"
     return true
 end
